17fa32f31c58914cae17fabe063fe3f8e63f5c18
OmarAltabbaa <https://github.com/OmarAltabbaa/vef2-2022-verkefni>
2025-04-03 21:09:03 +0000

    Create loading.tsx
    added loading statement to my rentals

---
+++ src/app/my-rentals/loading.tsx
@@ -0,0 +1,10 @@
+
+export default function LoadingMyRentals() {
+    return (
+      <main>
+        <h1>Loading your rentals...</h1>
+        <p>Please wait while we fetch your movie rentals </p>
+      </main>
+    );
+  }
+  
